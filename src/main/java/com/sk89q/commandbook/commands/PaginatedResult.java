6094e8e7740342d952d635a083c6b61cff20b9c5
Steve Anton <anxuiz.nx@gmail.com>
2012-07-19 14:30:07 -0700

    Add the PaginatedResult class from CommandBook, make some changes to it, and bump version to 0.2

---
+++ src/main/java/com/sk89q/commandbook/commands/PaginatedResult.java
@@ -0,0 +1,66 @@
+/*
+ * CommandBook
+ * Copyright (C) 2011 sk89q <http://www.sk89q.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.sk89q.commandbook.commands;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import org.bukkit.command.CommandSender;
+
+import com.sk89q.minecraft.util.commands.CommandException;
+
+/**
+ * Commands that wish to display a paginated list of results can use this class to do
+ * the actual pagination, giving a list of items, a page number, and basic formatting information.
+ */
+public abstract class PaginatedResult<T> {
+    protected final int resultsPerPage;
+
+    public PaginatedResult() {
+        this(6);
+    }
+
+    public PaginatedResult(int resultsPerPage) {
+        assert resultsPerPage > 0;
+        this.resultsPerPage = resultsPerPage;
+    }
+
+    public void display(CommandSender sender, Collection<? extends T> results, int page) throws CommandException {
+        this.display(sender, new ArrayList<T>(results), page);
+    }
+
+    public void display(CommandSender sender, List<? extends T> results, int page) throws CommandException {
+        if (results.size() == 0) throw new CommandException("No results match!");
+
+        int maxPages = results.size() / this.resultsPerPage + 1;
+        if (page <= 0 || page > maxPages) throw new CommandException("Unknown page selected! " + maxPages + " total pages.");
+
+        sender.sendMessage(this.formatHeader(page, maxPages));
+
+        for (int i = this.resultsPerPage * (page - 1); i < this.resultsPerPage * page && i < results.size(); i++) {
+            sender.sendMessage(this.format(results.get(i), i));
+        }
+    }
+
+    public abstract String formatHeader(int page, int maxPages);
+
+    public abstract String format(T entry, int index);
+}
+
---
+++ src/main/java/com/sk89q/commandbook/commands/SimplePaginatedResult.java
@@ -0,0 +1,22 @@
+package com.sk89q.commandbook.commands;
+
+import org.bukkit.ChatColor;
+
+public abstract class SimplePaginatedResult<T> extends PaginatedResult<T> {
+    protected final String header;
+
+    public SimplePaginatedResult(String header) {
+        super();
+        this.header = header;
+    }
+
+    public SimplePaginatedResult(String header, int resultsPerPage) {
+        super(resultsPerPage);
+        this.header = header;
+    }
+
+    @Override
+    public String formatHeader(int page, int maxPages) {
+        return ChatColor.YELLOW + this.header + " (page " + page + "/" + maxPages + ")";
+    }
+}
