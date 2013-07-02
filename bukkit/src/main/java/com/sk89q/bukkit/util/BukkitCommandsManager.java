31bf4a3ea3d5139808e2bed7639e4386103b6728
Isaac Moore <rmsy@me.com>
2013-07-02 18:35:33 -0500

    Add checkSender() to enforce @Console annotation

--- bukkit/src/main/java/com/sk89q/bukkit/util/BukkitCommandsManager.java
+++ bukkit/src/main/java/com/sk89q/bukkit/util/BukkitCommandsManager.java
@@ -1,12 +1,24 @@
 package com.sk89q.bukkit.util;
 
 import org.bukkit.command.CommandSender;
+import org.bukkit.command.ConsoleCommandSender;
 
+import com.sk89q.minecraft.util.commands.CommandException;
 import com.sk89q.minecraft.util.commands.CommandsManager;
+import com.sk89q.minecraft.util.commands.Console;
+
+import java.lang.reflect.Method;
 
 public class BukkitCommandsManager extends CommandsManager<CommandSender> {
     @Override
     public boolean hasPermission(CommandSender player, String perm) {
         return player.hasPermission(perm);
+    }
+
+    @Override
+    public void checkSender(CommandSender sender, Method method) throws CommandException {
+        if (!method.isAnnotationPresent(Console.class) && sender instanceof ConsoleCommandSender) {
+            throw new CommandException("You must be a player to execute this command.");
+        }
     }
 }
